4b7641f46d82a52ee70e9eead5a27ece8f94b127
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-01-08 16:52:09 +0900

    feat: BankAccount Contract
    https://betterprogramming.pub/developing-a-smart-contract-by-using-remix-ide-81ff6f44ba2f

---
+++ BankAccount.sol
@@ -0,0 +1,72 @@
+pragma solidity ^0.6.6;
+
+contract BankContract {
+    struct client_account {
+        int client_id;
+        address client_address;
+        uint client_balance_in_ether;
+    }
+    client_account[] clients;
+
+    int clientCounter;
+    constructor() public {
+        clientCounter = 0;
+    }
+
+    address payable manager;
+    mapping(address => uint) public interestDate;
+
+    modifier onlyManager() {
+        require(msg.sender == manager, "Only manager can call!");
+        _;
+    }
+
+    modifier onlyClients() {
+        bool isClient = false;
+        for (uint i = 0; i < clients.length; i++) {
+            if (clients[i].client_address == msg.sender) {
+                isClient = true;
+                break;
+            }
+        }
+        require(isClient, "only clients can call!!");
+        _;
+    }
+    
+    receive() external payable {}
+
+    function setManager(address managerAddress) public returns(string memory) {
+        manager = payable(managerAddress);
+        return "";
+    }
+
+    function joinAsClient() public payable returns(string memory) {
+        interestDate[msg.sender] = now;
+        clients.push(client_account(clientCounter++, msg.sender, address(msg.sender).balance));
+        return "";
+    }
+
+    function deposit() public payable onlyClients {
+        payable(address(this)).transfer(msg.value);
+    }
+
+    function withdraw(uint amount) public payable onlyClients {
+        msg.sender.transfer(amount * 1 ether);
+    }
+
+    function sendInterest() public payable onlyManager {
+        for (uint i = 0; i < clients.length; i++) {
+            address initialAddress = clients[i].client_address;
+            uint lastInterestDate = interestDate[initialAddress];
+            if (now < lastInterestDate + 10 seconds) {
+                revert("It's just been less than 10 second!");
+            }
+            payable(initialAddress).transfer(1 ether);
+            interestDate[initialAddress] = now;
+        }
+    }
+
+    function getContractBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+}
