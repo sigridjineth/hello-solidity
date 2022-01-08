47833a6986254054abc1de8dd5a51dfde196eea0
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-01-08 20:48:39 +0900

    feat: Mapping 연습

---
+++ MappingB.sol
@@ -0,0 +1,31 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract MappingB {
+    mapping(string => uint256) private priceList;
+    mapping(uint256 => string) private nameList;
+    mapping(uint256 => uint256) private ageList;
+
+    function setAgeList(uint256 _key, uint256 _age) public {
+        ageList[_key] = _age;
+    }
+    
+    function getAge(uint256 _key) public view returns(uint256) {
+        return ageList[_key];
+    }
+
+    function setNameList(uint256 _key, string memory _name) public {
+        nameList[_key] = _name;
+    }
+
+    function getName(uint256 _key) public view returns(string memory) {
+        return nameList[_key];
+    }
+
+    function setPriceList(string memory _itemName, uint256 _price) public {
+        priceList[_itemName] = _price;
+    }
+
+    function getPriceList(string memory _key) public view returns(uint256) {
+        return priceList[_key];
+    }
+}
