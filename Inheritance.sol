d590404a6b7239dd8e680740f4ac0de9558a987a
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-01-08 19:45:58 +0900

    update: virtual 키워드를 이용한 오버라이딩 함수 구현

--- Inheritance.sol
+++ Inheritance.sol
@@ -1,6 +1,7 @@
 pragma solidity >= 0.7.0 < 0.9.0;
 
 contract Snoop {
+    
     string public familyName = "snoop";
     string public givenName = "so";
     uint256 public money = 100; 
@@ -9,21 +10,32 @@ contract Snoop {
         givenName = _givenName;
     }
     
-    function getFamilyName() view public returns(string memory){
+    
+    function getFamilyName() view public  returns(string memory){
         return familyName;
     } 
     
-    function getGivenName() view public returns(string memory){
+    function getGivenName() view public  returns(string memory){
         return givenName;
     } 
     
-    function getMoney() view public returns(uint256){
+    // 먼저 오버라이딩을 하기 위해서는, 오버라이딩할 함수에 virtual 을 명시해주어야합니다.
+    function getMoney() view  public virtual returns(uint256){
         return money;
     }
     
-
+    
 }
 
 contract Jin is Snoop("Jin"){
-
+    
+    
+    uint256 public earning = 0;
+    function work() public {
+        earning += 100;
+    }
+    
+     function getMoney() view  public override returns(uint256){
+        return money+earning;
+    }
 }
